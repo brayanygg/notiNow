19174670770f7a5556d18b606f81e89cfd473fd0
CodeKra <123278481+CodeKra@users.noreply.github.com>
2023-01-31 21:21:52 -0400

    inicio

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-document.querySelector(".btnmenu").addEventListener("click", () => {
-    document.querySelector(".herramientas1").classList.toggle("show");
+const botonMenu = document.querySelector(".btnmenu")
+const bDesplegable = document.querySelector(".herramientas1")
+
+class noticia {
+  constructor(titulo, articulo, foto) {
+    this.titulo = titulo
+    this.articulo = articulo
+    this.foto = foto
+  }
+
+}
+function iniciar() {
+
+  botonMenu.addEventListener("click", () => {
+  bDesplegable.classList.toggle("show");
   });
+}
+window.addEventListener('load', iniciar())
